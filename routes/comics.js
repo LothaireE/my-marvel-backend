4c52f0a635cb6b045fff5054adaa0b73aedad7e8
LothaireE <lothaire.epee@gmail.com>
2022-02-17 21:27:41 +0100

    another lap around the clock

--- routes/comics.js
+++ routes/comics.js
@@ -5,11 +5,14 @@ const router = express.Router();
 const cors = require("cors");
 
 router.get("/comics", async (req, res) => {
-  const limit = 100;
-
+  console.log(req.query.title);
+  //   const limit = 100;
+  //   &limit=${limit}
+  const title = req.query.title;
   try {
+    console.log(title);
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MY_MARVEL_API_KEY}&limit=${limit}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MY_MARVEL_API_KEY}&title=${title}`
     );
     console.log("response===>", process.env.MY_MARVEL_API_KEY);
     res.status(200).json(response.data);
